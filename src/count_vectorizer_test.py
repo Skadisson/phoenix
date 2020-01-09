ddcfb37a589b87f877bcb25b7a3c488ca88ea547
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2020-01-09 10:34:49 +0100

    test: predicting relations

--- src/count_vectorizer_test.py
+++ src/count_vectorizer_test.py
@@ -1,29 +1,6 @@
 import os
 import pickle
-from sklearn import feature_extraction, datasets, naive_bayes
-
-"""
-categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
-twenty_train = datasets.fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
-count_vectorizer = feature_extraction.text.CountVectorizer()
-X_train_counts = count_vectorizer.fit_transform(twenty_train.data)
-tf_transformer = feature_extraction.text.TfidfTransformer(use_idf=False).fit(X_train_counts)
-X_train_tf = tf_transformer.transform(X_train_counts)
-tfidf_transformer = feature_extraction.text.TfidfTransformer()
-X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
-clf = naive_bayes.MultinomialNB().fit(X_train_tfidf, twenty_train.target)
-docs_new = ['God is love', 'OpenGL on the GPU is fast']
-X_new_counts = count_vectorizer.transform(docs_new)
-X_new_tfidf = tfidf_transformer.transform(X_new_counts)
-predicted = clf.predict(X_new_tfidf)
-print(X_train_counts.shape)
-print(count_vectorizer.vocabulary_.get(u'algorithm'))
-print(X_train_tf.shape)
-print(X_train_tfidf.shape)
-for doc, category in zip(docs_new, predicted):
-    print('%r => %s' % (doc, twenty_train.target_names[category]))
-exit()
-"""
+from sklearn import feature_extraction, naive_bayes
 
 cache_file = '../tmp/cache'
 file_exists = os.path.exists(cache_file)
