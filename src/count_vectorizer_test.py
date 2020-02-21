03d9508086ee94f94132711e9c68f303450827a7
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2020-02-21 16:04:50 +0100

    testing update

--- src/count_vectorizer_test.py
+++ src/count_vectorizer_test.py
@@ -2,10 +2,10 @@ import os
 import pickle
 from sklearn import feature_extraction, naive_bayes
 
-cache_file = '../tmp/cache'
-file_exists = os.path.exists(cache_file)
+jira_cache = '../tmp/jira'
+file_exists = os.path.exists(jira_cache)
 if file_exists:
-    file = open(cache_file, "rb")
+    file = open(jira_cache, "rb")
     content = pickle.load(file)
     descriptions = []
     keys = []
@@ -27,6 +27,22 @@ if file_exists:
         if description is not None and key is not None:
             keys.append(key)
             descriptions.append(str(description))
+
+    confluence_cache = '../tmp/confluence'
+    file_exists = os.path.exists(confluence_cache)
+    if file_exists:
+        file = open(confluence_cache, "rb")
+        confluence_content = pickle.load(file)
+        print(len(confluence_content))
+        exit()
+        for confluence_id in confluence_content:
+            document = confluence_content[confluence_id]
+            key = str(document['link'])
+            description = str(document['title'] + ' ' + document['text'])
+            if description is not None and key is not None:
+                keys.append(key)
+                descriptions.append(str(description))
+
     count_vectorizer = feature_extraction.text.CountVectorizer()
     X_train_counts = count_vectorizer.fit_transform(descriptions)
     tf_transformer = feature_extraction.text.TfidfTransformer(use_idf=False).fit(X_train_counts)
@@ -34,7 +50,7 @@ if file_exists:
     tfidf_transformer = feature_extraction.text.TfidfTransformer()
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
     clf = naive_bayes.MultinomialNB().fit(X_train_tfidf, keys)
-    docs_new = ['Wie konfiguriere ich die Rechte in brandbox 5 korrekt?', 'Der Flash Upload hat eine Macke', 'Ich hab da mal ein Problem mit SMTP auf Rancher', 'Hilfe der Server geht nicht', 'Arvato Mwst', 'Bei Frank-Flechtwaren stimmen die Bilder nicht']
+    docs_new = ['Wie funktioniert Quickbox brandbox checkout?', 'Wie konfiguriere ich die Rechte in brandbox 5 korrekt?', 'Der Flash Upload hat eine Macke', 'Ich hab da mal ein Problem mit SMTP auf Rancher', 'Hilfe der Server geht nicht', 'Arvato Mwst', 'Bei Frank-Flechtwaren stimmen die Bilder nicht']
     X_new_counts = count_vectorizer.transform(docs_new)
     X_new_tfidf = tfidf_transformer.transform(X_new_counts)
     predicted = clf.predict(X_new_tfidf)
