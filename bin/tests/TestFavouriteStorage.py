f91b1fc4f9ace86e373aaae9a452a25cf9564b8a
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2020-02-25 17:59:56 +0100

    [wip] Favourites

--- bin/tests/TestFavouriteStorage.py
+++ bin/tests/TestFavouriteStorage.py
@@ -3,6 +3,7 @@ from bin.entity import Card
 from bin.entity import User
 from bin.entity import Favourite
 import os
+import random
 
 
 class TestCardTransfer:
@@ -13,6 +14,39 @@ class TestCardTransfer:
 
     def run(self):
 
-        """TODO: TBI"""
+        users = self.create_random_users(10)
+        cards = self.create_random_cards(20)
+        clicks = 10000
+        expected_favourites = []
+
+
 
         return True
+
+    def create_random_cards(self, length):
+        cards = []
+        for card_id in range(1, length):
+            cards.append(self.create_random_card(card_id))
+        return cards
+
+    @staticmethod
+    def create_random_card(card_id):
+        card = Card.Card()
+        card.id = card_id
+        card.likes = 0
+
+        return card
+
+    def create_random_users(self, length):
+        users = []
+        for user_id in range(1, length):
+            users.append(self.create_random_user(user_id))
+        return users
+
+    @staticmethod
+    def create_random_user(user_id):
+        user = User.User()
+        user.id = user_id
+        user.alias = random.getrandbits(3)
+
+        return user
