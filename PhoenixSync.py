409f6f6b8f428bfce677ab3608b135c361b6b120
Sebastian Schulz <skadisson@mailbox.org>
2020-06-19 00:59:40 +0200

    lots of optimizations and implemented gitlab toggle

--- PhoenixSync.py
+++ PhoenixSync.py
@@ -2,10 +2,12 @@ from bin.service import ConfluenceAPI
 from bin.service import Gitlab
 from bin.service import Jira
 from bin.service import Logger
+from bin.service import Environment
 import _thread
 import threading
 
 logger = Logger.Logger()
+environment = Environment.Environment()
 try:
 
     def confluence_thread():
@@ -27,7 +29,8 @@ try:
         gitlab = Gitlab.Gitlab()
         gitlab.sync_commits(0)
 
-    _thread.start_new_thread(git_thread, ())
+    if environment.get_service_enable_git():
+        _thread.start_new_thread(git_thread, ())
 
     while threading.activeCount():
         pass
