612a209606fb44fd0ce4fe69f48069279a608ede
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2021-07-12 08:59:20 +0200

    webserver port changed

--- www/server.py
+++ www/server.py
@@ -2,7 +2,7 @@ import http.server
 import socketserver
 
 
-PORT = 8090
+PORT = 8110
 Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
