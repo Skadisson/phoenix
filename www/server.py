1dbb4254c2c0b7555fa9ac66fc1a94894b2beeaa
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2021-07-14 13:20:13 +0200

    added ssl

--- www/server.py
+++ www/server.py
@@ -1,5 +1,6 @@
 import os
 import yaml
+import ssl
 import http.server
 import socketserver
 
@@ -10,5 +11,6 @@ PORT = service_yaml['port_webserver']
 Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
+    httpd.socket = ssl.wrap_socket(httpd.socket, server_side=True, certfile='../localhost.pem', ssl_version=ssl.PROTOCOL_TLSv1_2)
     httpd.serve_forever()
     Handler.send_header('Access-Control-Allow-Origin', '*')
