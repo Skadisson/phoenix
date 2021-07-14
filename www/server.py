9de3ccd9c13b8a35ca19a1d6349bad16497fb7b8
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2021-07-14 13:08:13 +0200

    retracted package from webserver

--- www/server.py
+++ www/server.py
@@ -1,9 +1,12 @@
+import os
+import yaml
 import http.server
 import socketserver
-import bin.service.Environment
 
-env = bin.service.Environment.Environment()
-PORT = env.get_server_port()
+path = os.path.join("..", "env", "service.yaml")
+file = open(path, "r", encoding='utf8')
+service_yaml = yaml.load(file, Loader=yaml.FullLoader)
+PORT = service_yaml['port_webserver']
 Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
