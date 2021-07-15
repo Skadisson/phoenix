e29e9371c91e634ab04712ef6156612771f55839
Sebastian Schulz <72b9feddc62a5b8c33ba6da300a9ea75>
2021-07-15 19:42:37 +0200

    click tracking fixed

--- www/main.js
+++ www/main.js
@@ -679,6 +679,17 @@ PS = (function(window, document, $) {
         $('.author', $template).text(author);
         $('#link-list').append($template);
         $($template).off('click');
+        $($template).on('click', function(event) {
+            event.preventDefault();
+            var external_link = $('p', $template).attr('data-card-link');
+            var card_id = $('p', $template).attr('data-card-id');
+            var query = $('#keywords').val();
+            var getUrl = host_protocol + '://' + host_name + ':' + host_port + '/?function=Click&card_id=' + encodeURIComponent(card_id) + '&query=' + encodeURIComponent(query) + '&loading_seconds=' + encodeURIComponent(loading_seconds) + '&frontend=desktop';
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', getUrl, true);
+            xhr.send();
+            window.open(external_link);
+        });
         $('.edit', $template).on('click', function(event) {
             event.preventDefault();
             self.register_events();
